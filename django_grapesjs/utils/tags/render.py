a3db8f4b3166cab30f5daa5aa1fb475ebbcc5fd2
illantal <illantalex84@gmail.com>
2021-05-01 20:23:05 +0300

    added render with lxml

--- django_grapesjs/utils/tags/render.py
+++ django_grapesjs/utils/tags/render.py
@@ -1,20 +1,42 @@
-import re
-from functools import reduce
+# import re
+# from functools import reduce
 from django.template import Template, Context
 from django_grapesjs.settings import NAME_RENDER_TAG
-
+from lxml import html
+# NAME_RENDER_TAG = "render"
 __all__ = ('ApplyRenderTag', )
 
 
 REGEX_RENDER_TAG = '<%s>(.*?)</%s>' % (NAME_RENDER_TAG, NAME_RENDER_TAG)
-
+print(REGEX_RENDER_TAG)
 
 class ApplyRenderTag(object):
     def apply_tag_init(self, string):
-        strings_to_render = re.findall(REGEX_RENDER_TAG, string)
-        replace_to_strings = map(lambda t: t.render(Context({})), map(Template, strings_to_render))
+        document = html.fromstring(string)
+        # print(NAME_RENDER_TAG)
+        for tag in document.xpath(f"//{NAME_RENDER_TAG}"):
+            # tag.replace(tag, html.fromstring(Template(html.tostring(tag, encoding="unicode")).render(Context({}))))
 
-        return reduce(lambda s, r: re.sub(REGEX_RENDER_TAG, r, s, 1), replace_to_strings, string)
+            tag.getparent().append(html.fromstring(Template(html.tostring(tag, encoding="unicode")).render(Context({}))))
+            # tag.tail = "<render><h1>Hello</h1></render>"
+
+            # print(tag.tail)
+        # etree.strip_elements(document, NAME_RENDER_TAG, with_tail=False)
+        # print(html.tostring(document, pretty_print=True).decode("utf-8"))
+
+        return html.tostring(document, pretty_print=True, encoding="unicode", method="html")
+
 
     def apply_tag_save(self, string):
         return string
+
+# data = """<document>
+# <h1>
+# Heading
+# </h1>
+# <render>
+# <p>p1</p>
+# <p>p2</p>
+# </render>
+# </document>
+# """
--- example/app/models.py
+++ example/app/models.py
@@ -1,7 +1,7 @@
+from django_grapesjs.settings import GRAPESJS_DEFAULT_HTML
 from django.db import models
 from django_grapesjs.models import GrapesJsHtmlField
 
 
 class ExampleModel(models.Model):
-    html = GrapesJsHtmlField(apply_django_tag=True)
-
+    html = GrapesJsHtmlField(apply_django_tag=False, default_html="test.html")
